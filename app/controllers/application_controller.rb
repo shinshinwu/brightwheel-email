294b8553ba11413b4dccd4fe557f74a23d65c265
Anna Wu <shinshinwu@gmail.com>
2017-02-05 23:42:37 -0800

    add before validation filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 # putting everything here since it seems to be bit overkill to create its own controller for 1 route. If app scales up, please seperate out to it's own controller
+
+# TODO: make api version control
+
 require "email_services"
 
 class ApplicationController < ActionController::API
 
-  before_filter :authenticate_request
+  before_action :authenticate_request
+  before_action :validate_params
 
   rescue_from StandardError, with: :_include_error_info_in_response
 
@@ -23,16 +27,34 @@ class ApplicationController < ActionController::API
   #   “body”: “<h1>Are you</h1><p>her?</p>”
   # }
 
-  def email
+  def send_email
+
+
   end
 
   private
 
   def authenticate_request
     # in full scale app, you would probably would want to authenticate request against real user database
-    raise EmailServices::UnauthenticatedError, "Sorry your request is unauthenticated" unless params[:token] != "wearebright"
+    raise EmailServices::UnauthenticatedError, "Sorry your request is unauthenticated." unless params[:token] == "wearebright"
   end
 
+  def validate_params
+    missing_params = EmailServices::REQUIRED_FIELDS - params.keys
+    if missing_params.present?
+      raise EmailServices::MissingFieldsError, missing_params
+    end
+
+    # ideally, you would want to validate the email against user table and make sure they don't have email notification turned off
+    unless EmailServices::valid_email?(params[:to])
+      raise EmailServices::InvalidEmailError, "We can not validate to email address, please double check your input and try again."
+    end
+
+    # make sure the email send to your users is from a permitted and valid org email
+    unless Settings.permitted_org_emails.include?(params[:from])
+      raise EmailServices::InvalidEmailError, "Sorry, your from email input is not from an authorized address."
+    end
+  end
 
   def _render_response(status_code=nil)
     render({ json: @response, status: status_code || 200 })
@@ -41,7 +63,7 @@ class ApplicationController < ActionController::API
 
   def _include_error_info_in_response(exception)
     @response[:errors]            ||= {}
-    wrapper = ActionDispatch::ExceptionWrapper.new(Rails.env, exception)
+    wrapper = ActionDispatch::ExceptionWrapper.new(nil, exception)
     trace   = wrapper.application_trace
     trace   = wrapper.framework_trace if trace.empty?
 
--- lib/email_services.rb
+++ lib/email_services.rb
@@ -1,5 +1,7 @@
 module EmailServices
 
+  REQUIRED_FIELDS = ["to", "to_name", "from", "from_name", "subject", "body"]
+
   class EmailServiceError < StandardError
     def initialize(message="")
       super(message)
@@ -7,5 +9,20 @@ module EmailServices
   end
 
   class UnauthenticatedError < EmailServiceError; end
+  class MissingFieldsError < EmailServiceError
+    def initialize(missing_fields)
+      super("Missing required inputs for #{missing_fields.to_sentence} fields. Please compelete all required fields and try again.")
+    end
+  end
+
+  class InvalidEmailError < EmailServiceError
+  end
+
+  class self.valid_email?(email_address)
+    invalid_email_chars  = /\A\s*([-\p{L}\d+._]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/i
+
+    # regex return 0 if email input is valid
+    (email =~ invalid_email_chars) == 0
+  end
 
 end
