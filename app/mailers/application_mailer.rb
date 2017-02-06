14268e996ce1c85b7d6e4377ca01c7a16d5a1bf6
Anna Wu <shinshinwu@gmail.com>
2017-02-06 14:44:28 -0800

    update mailer to dynamically pull smtp info

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,31 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'support@brightwheel.com'
   layout 'mailer'
+
+  include ActionView::Helpers::SanitizeHelper
+
+  def notification(email_params, email_service=nil)
+    recipient = email_params[:to]
+    sender    = email_params[:from]
+
+    subject   = email_params[:subject]
+    body      = strip_tags(email_params[:body])
+
+    email_service ||= Settings.default_email_service
+
+    smtp_credentials = {
+      user_name: ENV["#{email_service.upcase}_USER"],
+      password:  ENV["#{email_service.upcase}_PW"]
+    }
+
+    # load other smtp params from settings file such as port and address etc
+    delivery_options = smtp_credentials.merge(Settings[email_service])
+
+    mail(to:                      recipient,
+         from:                    sender,
+         subject:                 subject,
+         body:                    body,
+         delivery_method_options: delivery_options
+        )
+  end
 end
